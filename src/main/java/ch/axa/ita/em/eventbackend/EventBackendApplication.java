bece17d19d5f39c71f1e521e19ea87246edb66e3
Eleni Müller <eleni.mueller@axa.ch>
2024-08-05 11:08:16 +0200

    first commit

---
+++ src/main/java/ch/axa/ita/em/eventbackend/EventBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.axa.ita.em.eventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBackendApplication.class, args);
+	}
+
+}
